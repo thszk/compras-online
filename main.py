c719e8ecaa0858e3162dfde4952fc667706096e4
thszk <thsuzuqui@gmail.com>
2024-08-13 20:07:39 -0400

    adicionando menu do admin

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import controller.produto_controller as produto_controller
 import controller.cliente_controller as cliente_controller
 
-
-produto_controller.cadastrar()
-produto_controller.listar()
-
-
 cliente_controller.cadstrar()
 cliente_controller.adicionar_no_carrinho()
+
+def menu_admin():
+    while True:
+        print('Menu Admin')
+        print('1. Cadastro produto')
+        print('2. Listar produto')
+        print('0. Voltar')
+
+        op_admin = int(input('> '))
+
+        match op_admin:
+            case 1:
+                produto_controller.cadastrar()
+            case 2:
+                produto_controller.listar()
+            case 0:
+                break
+
+
+menu_admin()
